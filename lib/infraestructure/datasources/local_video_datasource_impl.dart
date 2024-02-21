cc4dd05c6103e177444912a37061d078f3284007
Xlart5 <brayanterceros@gmail.com>
2024-02-21 08:38:03 -0400

    seccond commit

---
+++ lib/infraestructure/datasources/local_video_datasource_impl.dart
@@ -0,0 +1,21 @@
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infraestructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
+
+class LocalVideoDatasource extends VideoPostDatasource {
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    // TODO: implement getFavoriteVideosByUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) async {
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideosPostEntity())
+        .toList();
+    return newVideos;
+  }
+}
